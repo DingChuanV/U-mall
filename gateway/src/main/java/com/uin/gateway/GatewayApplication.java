c4a0859dd609ea6e671b6516ae51b0dadf3b9a75
wanglufei <2686221966@qq.com>
2022-04-19 21:21:32 +0800

    使用gateway做API网关并完成测试

---
+++ gateway/src/main/java/com/uin/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.uin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//开启服务发现注册
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
