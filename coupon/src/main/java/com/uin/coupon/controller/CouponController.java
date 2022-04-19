1ec67417e70cf70d22636e3356cf11930b9b1e35
wanglufei <2686221966@qq.com>
2022-04-19 15:41:15 +0800

    使用nacos做配置中心完成测试

--- coupon/src/main/java/com/uin/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/uin/coupon/controller/CouponController.java
@@ -9,6 +9,7 @@ import com.uin.coupon.service.CouponService;
 import com.uin.utils.PageUtils;
 import com.uin.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -27,6 +28,14 @@ public class CouponController {
 
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     /**
      * 测试远程服务调用
