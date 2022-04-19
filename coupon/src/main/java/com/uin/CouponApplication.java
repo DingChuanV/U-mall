79c17fd61c6b57b2b97b17c5550d1b79ec34cc05
wanglufei <2686221966@qq.com>
2022-04-19 12:42:41 +0800

    使用nacos注册优惠券服务

--- coupon/src/main/java/com/uin/CouponApplication.java
+++ coupon/src/main/java/com/uin/CouponApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//开启服务注册与发现功能
+@EnableDiscoveryClient
 @MapperScan("com.uin.coupon.dao")
 public class CouponApplication {
 
