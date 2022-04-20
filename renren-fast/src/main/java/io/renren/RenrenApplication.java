019961ee2bdd40098ae24f1f835066aa2e0c5775
wanglufei <2686221966@qq.com>
2022-04-20 21:12:23 +0800

    使用gateway网关进行路由

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -10,9 +10,13 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableWebMvc
 public class RenrenApplication {
 
 	public static void main(String[] args) {
