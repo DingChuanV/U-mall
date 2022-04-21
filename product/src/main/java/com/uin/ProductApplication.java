274a41029e7b3393959b57ca1b0e78df8c2fb8cc
wanglufei <2686221966@qq.com>
2022-04-21 09:09:28 +0800

    完成gateway路由到商品服务并完成测试

--- product/src/main/java/com/uin/ProductApplication.java
+++ product/src/main/java/com/uin/ProductApplication.java
@@ -3,8 +3,11 @@ package com.uin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @MapperScan("com.uin.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
 
     public static void main(String[] args) {
