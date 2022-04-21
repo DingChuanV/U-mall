5793a0c582a3eee82c615bb16891cd81aa17e96b
wanglufei <2686221966@qq.com>
2022-04-21 21:26:39 +0800

    oss邮件警告

---
+++ thirdPaty/src/main/java/com/uin/thirdpaty/ThirdPatyApplication.java
@@ -0,0 +1,13 @@
+package com.uin.thirdpaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPatyApplication.class, args);
+	}
+
+}
---
+++ thirdPaty/src/test/java/com/uin/thirdpaty/ThirdPatyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uin.thirdpaty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPatyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
