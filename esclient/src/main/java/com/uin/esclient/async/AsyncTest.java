13c0a3d66b0b5ab373192d0b3932e99fd5234b06
wanglufei <2686221966@qq.com>
2022-09-19 16:15:40 +0800

    商城业务--复习创建线程的4种方式

--- esclient/src/main/java/com/uin/esclient/async/AsyncTest.java
+++ esclient/src/main/java/com/uin/esclient/async/AsyncTest.java
@@ -1,7 +1,6 @@
 package com.uin.esclient.async;
 
 import lombok.extern.slf4j.Slf4j;
-import net.sf.jsqlparser.statement.execute.Execute;
 
 import java.util.concurrent.*;
 
@@ -10,6 +9,7 @@ public class AsyncTest {
     public static ExecutorService service = Executors.newFixedThreadPool(5);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+
         //1.Thread
         //Thread thread = new ThreadDemo();
         //thread.start();
@@ -36,6 +36,40 @@ public class AsyncTest {
          * 1. 1/2没有返回值 3可以有返回值
          * 2. 1/2/3 比较浪费资源 4可以控制，性能稳定
          */
+        /**
+         * Creates a new {@code ThreadPoolExecutor} with the given initial
+         * parameters.
+         *
+         * @param corePoolSize the number of threads to keep in the pool, even
+         *        if they are idle, unless {@code allowCoreThreadTimeOut} is set
+         * @param maximumPoolSize the maximum number of threads to allow in the
+         *        pool
+         * @param keepAliveTime when the number of threads is greater than
+         *        the core, this is the maximum time that excess idle threads
+         *        will wait for new tasks before terminating.
+         * @param unit the time unit for the {@code keepAliveTime} argument
+         * @param workQueue the queue to use for holding tasks before they are
+         *        executed.  This queue will hold only the {@code Runnable}
+         *        tasks submitted by the {@code execute} method.
+         * @param threadFactory the factory to use when the executor
+         *        creates a new thread
+         * @param handler the handler to use when execution is blocked
+         *        because the thread bounds and queue capacities are reached
+         * @throws IllegalArgumentException if one of the following holds:<br>
+         *         {@code corePoolSize < 0}<br>
+         *         {@code keepAliveTime < 0}<br>
+         *         {@code maximumPoolSize <= 0}<br>
+         *         {@code maximumPoolSize < corePoolSize}
+         * @throws NullPointerException if {@code workQueue}
+         *         or {@code threadFactory} or {@code handler} is null
+         */
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5,
+                200,
+                2,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(10),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
 
     }
 
